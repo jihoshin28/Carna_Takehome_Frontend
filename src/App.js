117967116745d737b47aa6c0e8688147d10280bc
Allen <jihoshin.28@gmail.com>
2021-05-13 16:09:20 -0700

    added page imports to app file with routes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,33 @@
-import Navbar from './containers/Navbar'
 import './App.css';
 import {Route} from 'react-router-dom'
 import { connect } from 'react-redux'
+import AuthScreen from './screens/AuthScreen'
+import CalendarScreen from './screens/CalendarScreen'
+import ChartsScreen from './screens/ChartsScreen'
+import CoursesScreen from './screens/CoursesScreen'
+import ForumsScreen from './screens/ForumsScreen'
+import ProfileScreen from './screens/ProfileScreen'
+import SettingsScreen from './screens/SettinsScreen'
+import UsersScreen from './screens/UsersScreen'
+import NavBar from './containers/Navbar'
+
 
 function App() {
   return (
     <div className="App">
-      <NavBar history = {this.props.history}/>  
-        <Route exact path={'/'} render={(props) => <Home {...props} />}></Route> 
-        <Route exact path={'/order_history'} render={(props) => <OrderHistory {...props} />} />
-        <Route exact path={'/active_orders'} render={(props) => <ActiveOrders {...props} />} />
-        <Route exact path={'/about'} render={(props) => <About {...props} />} />
-        <Route exact path={'/cart'} render={(props) => <Cart {...props} />} />
+      <div>
+
+        <NavBar history = {this.props.history}/>  
+          <Route exact path={'/'} render={(props) => <AdminPanel {...props} />}></Route> 
+          <Route exact path={'/auth'} render={(props) => <AuthScreen {...props} />} />
+          <Route exact path={'/calendar'} render={(props) => <CalendarScreen {...props} />} />
+          <Route exact path={'/charts'} render={(props) => <ChartsScreen {...props} />} />
+          <Route exact path={'/courses'} render={(props) => <CoursesScreen {...props} />} />
+          <Route exact path={'/forums'} render={(props) => <ForumsScreen {...props} />} />
+          <Route exact path={'/profile'} render={(props) => <ProfileScreen {...props} />} />
+          <Route exact path={'/settings'} render={(props) => <SettingsScreen {...props} />} />
+          <Route exact path={'/users'} render={(props) => <UsersScreen {...props} />} />
+      </div>
         
     </div>
   );
---
+++ src/screens/AdminPanel.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class AdminPanel extends React.Component {
+    render(){
+        return(
+            <div>
+                <h1>Admin Panel</h1>
+            </div>
+        )
+    }
+}
+
+export default AdminPanel
