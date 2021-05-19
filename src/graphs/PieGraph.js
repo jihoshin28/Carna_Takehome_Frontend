0f6bbc9b3f350889f219b9c4b9e1839009d50e46
Allen <jihoshin.28@gmail.com>
2021-05-19 14:54:17 -0700

    cimported google charts graphs

---
+++ src/graphs/PieGraph.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import Chart from 'react-google-charts'
+
+class BarGraph extends React.Component{
+    render(){
+        return(
+            <div>
+                <Chart
+                width={'500px'}
+                height={'300px'}
+                chartType="PieChart"
+                loader={<div>Loading Chart</div>}
+                data={[
+                    ['Task', 'Hours per Day'],
+                    ['Work', 11],
+                    ['Eat', 2],
+                    ['Commute', 2],
+                    ['Watch TV', 2],
+                    ['Sleep', 7],
+                ]}
+                options={{
+                    title: 'My Daily Activities',
+                }}
+                rootProps={{ 'data-testid': '1' }}
+                />
+            </div>
+        )
+    }
+}
+
+
+export default BarGraph
